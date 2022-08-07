8a66c0584d3387c5d66050abb459730eaabf819e
jezmu <jezmuenterprises@gmail.com>
2022-08-07 02:39:16 -0700

    made changes

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,16 +1,41 @@
 #!/usr/bin/python3
-""" testing Amenity """
+"""
+Test suits for amenities
+"""
+import os
+import models
 import unittest
-import pep8
+from datetime import datetime
 from models.amenity import Amenity
 
-class Amenity_testing(unittest.TestCase):
-    """ check BaseModel """
 
-    def testpep8(self):
-        """ testing codestyle """
-        pepstylecode = pep8.StyleGuide(quiet=True)
-        path_user = 'models/amenity.py'
-        result = pepstylecode.check_files([path_user])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings).")
+class TestAmenity(unittest.TestCase):
+    """Tests instances and methods from amenity class"""
+
+    a = Amenity()
+
+    def test_class_exists(self):
+        """tests if class exists"""
+        res = "<class 'models.amenity.Amenity'>"
+        self.assertEqual(str(type(self.a)), res)
+
+    def test_user_inheritance(self):
+        """test if Amenity is a subclass of BaseModel"""
+        self.assertIsInstance(self.a, Amenity)
+
+    def testHasAttributes(self):
+        """verify if attributes exist"""
+        self.assertTrue(hasattr(self.a, 'name'))
+        self.assertTrue(hasattr(self.a, 'id'))
+        self.assertTrue(hasattr(self.a, 'created_at'))
+        self.assertTrue(hasattr(self.a, 'updated_at'))
+
+    def test_types(self):
+        """tests if the type of the attribute is the correct one"""
+        self.assertIsInstance(self.a.name, str)
+        self.assertIsInstance(self.a.id, str)
+        self.assertIsInstance(self.a.created_at, datetime.datetime)
+        self.assertIsInstance(self.a.updated_at, datetime.datetime)
+
+if __name__ == '__main__':
+    unittest.main()

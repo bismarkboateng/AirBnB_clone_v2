c88f1f036434084b37817ac6d62266bf2fccd4e5
bismarkboateng <bismarkb609@gmail.com>
2022-10-16 03:48:45 -0700

    c is fun

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -22,7 +22,7 @@ def hbnb():
 @app.route("/c/<text>", strict_slashes=False)
 def c_is_fun(text):
     """ echos back a variable passed to it at /c/text """
-    return "C %s" % escape(text.replace("_", ""))
+    return "C %s" % escape(text.replace("_", " "))
 
 
 if __name__ == "__main__":
